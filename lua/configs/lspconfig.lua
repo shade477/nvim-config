4917a4d6e6e0f6e56279d5658f3b4bb762dcaf25
shade477 <sagardeyof477@gmail.com>
2024-09-22 14:41:39 +0530

    Initial config

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,42 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
+
+-- EXAMPLE
+local servers = { "lua_lsp" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = require("plugins.configs.lspconfig").capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      usePlaceHolders = true,
+      completeUnimported = true,
+      analyses = {
+        unusedparams = true,
+      }
+    },
+  }
+}
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
